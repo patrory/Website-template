f52654c2ac34c3e22240147ec2f38002059c3882
pratik <21pratikdhane@gmail.com>
2022-12-26 17:26:29 +0530

    Login backend conneceted with frontend

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Spinner() {
+    return (
+        <div className='loadingSpinnerContainer'>
+            <div className="loadingspinner"></div>
+        </div>
+    );
+}
+
+export default Spinner;
